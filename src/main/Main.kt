f7b45febf96a1f74c993c1f315d383997fed8675
neelkamath <neelkamath@icloud.com>
2019-08-09 15:15:49 +0530

    fuck

--- src/main/Main.kt
+++ src/main/Main.kt
@@ -1,5 +1,6 @@
 package com.neelkamath.crystalskull
 
+import com.neelkamath.kwikipedia.search
 import io.ktor.application.Application
 import io.ktor.application.call
 import io.ktor.application.install
@@ -12,9 +13,10 @@ import io.ktor.routing.get
 
 // TODO: Test that the documentation link the GitHub UI and README are correct after GitLab Pages has been deployed.
 // TODO: Update deploy instructions in fork.md, .gitlab-ci.yml, and README.md. Credit that blog if it worked.
+// TODO: Test that all the fork and README instructions actually work
 
 fun Application.main() {
     install(CallLogging)
     install(ContentNegotiation) { gson { } }
-    install(Routing) { get("search") { call.respond("Hollo Bob") } } // TODO: Revert after testing Docker pushes.
+    install(Routing) { get("search") { call.respond(search(call.request.queryParameters["query"]!!)) } }
 }
