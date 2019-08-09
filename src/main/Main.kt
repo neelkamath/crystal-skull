3067b3b5c3aa6f19771ff747ed8e3f83af8a4138
neelkamath <neelkamath@icloud.com>
2019-08-09 16:00:18 +0530

    dind

--- src/main/Main.kt
+++ src/main/Main.kt
@@ -12,7 +12,7 @@ import io.ktor.routing.Routing
 import io.ktor.routing.get
 
 // TODO: Test that the documentation link the GitHub UI and README are correct after GitLab Pages has been deployed.
-// TODO: Update deploy instructions in fork.md, .gitlab-ci.yml, and README.md. Credit that blog if it worked.
+// TODO: Update deploy instructions in fork.md, .gitlab-ci.yml, and README.md.
 // TODO: Test that all the fork and README instructions actually work
 
 fun Application.main() {
