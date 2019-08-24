0459bb66e4c46b8c4accf5c3b68bd285b78f8b9f
neelkamath <neelkamath@icloud.com>
2019-08-24 20:41:41 +0530

    Remove duplicate options

--- src/main/Quiz.kt
+++ src/main/Quiz.kt
@@ -27,8 +27,14 @@ internal fun createQuestions(
 private fun ask(processed: ProcessedSentence, options: Set<String>, answer: String): QuestionAnswer = QuestionAnswer(
     QuestionContext(processed.context.sentence.replaceFirst(answer, "_____"), processed.context.previous),
     (options.shuffled().take(3) + answer)
-        .toMutableSet()
-        .apply { while (size < 4) add(getRandomEntity(processed.entity)) }
+        .let {
+            var set = removeDuplicates(it.toSet(), answer).toMutableSet()
+            while (set.size < 4) {
+                set.add(getRandomEntity(processed.entity))
+                set = removeDuplicates(set, answer).toMutableSet()
+            }
+            set
+        }
         .shuffled()
         .toSet(),
     answer
@@ -57,4 +63,25 @@ internal fun getRandomEntity(entity: NamedEntity): String = with(Faker()) {
 internal fun getRandomTime(): String = with(Calendar.getInstance().apply { time = Faker().date().birthday() }) {
     val format = { value: Int -> "${if (value < 10) "0" else ""}$value" }
     "${(format(get(Calendar.HOUR) + 1))}:${format(get(Calendar.MINUTE))} ${listOf("AM", "PM").random()}"
+}
+
+/**
+ * Removes duplicate [strings] so that the [sought] after one is unique.
+ *
+ * Let's take an example where the strings are `"Steve Paul Jobs"`, `"Steve"`, `"Steve Wozniak"`, `
+ * "Steve Gary Wozniak"`, `"Gil Steve"`, and `"Gil Amelio"`. `"Steve"` and `"Steve Gary Wozniak"` will be removed if
+ * the [sought] after string is `"Steve Wozniak"`. `"Steve Paul Jobs"`, `"Steve Wozniak"`, `"Steve Gary Wozniak"`, and
+ * `"Gil Steve"` will be removed if `"Steve"` is the [sought] after string. Nothing will be removed if `"Gil Amelio"` is
+ * the [sought] after string.
+ */
+internal fun removeDuplicates(strings: Set<String>, sought: String): Set<String> {
+    val splits = strings.map { it.split(" ") }
+    val unique = mutableSetOf(sought)
+    val soughtSplit = sought.split(" ")
+    for (split in splits) {
+        if (split != soughtSplit && !soughtSplit.containsAll(split) && !split.containsAll(soughtSplit)) {
+            unique.add(split.joinToString(" "))
+        }
+    }
+    return unique
 }
--- src/test/QuizTest.kt
+++ src/test/QuizTest.kt
@@ -3,7 +3,10 @@ package com.neelkamath.crystalskull.test
 import com.neelkamath.crystalskull.NamedEntity
 import com.neelkamath.crystalskull.getRandomEntity
 import com.neelkamath.crystalskull.getRandomTime
+import com.neelkamath.crystalskull.removeDuplicates
 import io.kotlintest.matchers.string.shouldMatch
+import io.kotlintest.matchers.withClue
+import io.kotlintest.shouldBe
 import io.kotlintest.specs.StringSpec
 
 class EntityGeneratorTest : StringSpec({
@@ -20,3 +23,29 @@ class EntityGeneratorTest : StringSpec({
 class TimeGeneratorTest : StringSpec({
     "Randomly generated times must look real" { getRandomTime() shouldMatch Regex("""\d\d:\d\d [AP]M""") }
 })
+
+class DuplicateRemoverTest : StringSpec() {
+    init {
+        val testSet =
+            setOf("Steve Paul Jobs", "Steve", "Steve Wozniak", "Steve Gary Wozniak", "Gil Steve", "Gil Amelio")
+        val test = { set: Set<String>, sought: String, duplicates: Set<String> ->
+            withClue("Sought: $sought") { removeDuplicates(set, sought) shouldBe set - duplicates }
+        }
+
+        "Smaller duplicates must be removed when a bigger string is sought" {
+            test(testSet, "Steve Wozniak", setOf("Steve", "Steve Gary Wozniak"))
+        }
+
+        "Bigger duplicates must be removed when a smaller string is sought" {
+            test(testSet, "Steve", setOf("Steve Paul Jobs", "Steve Wozniak", "Steve Gary Wozniak", "Gil Steve"))
+        }
+
+        "String containing unique terms mustn't be removed when there are no duplicates" {
+            test(setOf("Neel K.", "Pasquale S.", "Lord G."), "Neel K.", setOf())
+        }
+
+        "Strings containing the same terms mustn't be removed when there are no duplicates" {
+            test(testSet, "Gil Amelio", setOf())
+        }
+    }
+}
