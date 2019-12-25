6e7408adf46f7b49d74c785e3bfe72a04104de52
Neel Kamath <neelkamath@icloud.com>
2019-12-25 10:50:44 +0530

    Fix #32 (#33)
    * Use /sense2vec endpoint instead of /ner
    
    * Simplify
    
    * Fix socket timeout exception

--- src/main/NLP.kt
+++ src/main/NLP.kt
@@ -14,7 +14,7 @@ import java.util.concurrent.TimeUnit
 object NLP {
     private val service = Retrofit.Builder()
         .baseUrl(System.getenv("SPACY_SERVER_URL"))
-        .client(OkHttpClient.Builder().readTimeout(2, TimeUnit.MINUTES).build())
+        .client(OkHttpClient.Builder().readTimeout(5, TimeUnit.MINUTES).build())
         .addConverterFactory(GsonConverterFactory.create(gson))
         .build()
         .create(NLPService::class.java)
@@ -33,6 +33,10 @@ object NLP {
     )
 
     private data class NERResponse(val data: List<NamedEntity>)
+
+    private data class Sense2vecRequest(val sentence: String, val phrase: String)
+
+    private data class Sense2vecResponse(val sense2vec: List<SimilarPhrase>)
 
     /** The recognized named [entities] in the sentence's [text]. */
     data class NamedEntity(val entities: List<RecognizedEntity>, val text: String)
@@ -71,6 +75,9 @@ object NLP {
         @POST("ner")
         fun recognizeNamedEntities(@Body request: NERRequest): Call<NERResponse>
 
+        @POST("sense2vec")
+        fun sense2vec(@Body request: Sense2vecRequest): Call<Sense2vecResponse>
+
         @POST("tokenizer")
         fun tokenize(@Body request: TokenizerRequest): Call<TokenizerResponse>
 
@@ -84,8 +91,10 @@ object NLP {
     fun recognizeNamedEntities(strings: List<String>, useSense2vec: Boolean = false): List<NamedEntity> =
         service.recognizeNamedEntities(NERRequest(strings, useSense2vec)).execute().body()!!.data
 
-    fun tokenize(text: String): List<String> =
-        service.tokenize(TokenizerRequest(text)).execute().body()!!.tokens
+    fun sense2vec(sentence: String, phrase: String): List<SimilarPhrase> =
+        service.sense2vec(Sense2vecRequest(sentence, phrase)).execute().body()!!.sense2vec
+
+    fun tokenize(text: String): List<String> = service.tokenize(TokenizerRequest(text)).execute().body()!!.tokens
 
     fun sentencize(data: String): List<String> =
         service.sentencize(SentencizerRequest(data)).execute().body()!!.sentences
