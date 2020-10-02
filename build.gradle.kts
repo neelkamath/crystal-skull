ae17cf7114c5b5091a026ed0ba07f1dceb7b905f
Juan Miguel Boyero Corral <juanmi1982@gmail.com>
2020-10-02 08:44:53 +0200

    Update project dependencies. Fixes #35 (#36)
    * Update project dependencies. Fixes #35
    
    * Fix ports syntax in docker-compose.yml
    
    * Fix Dockerfile
    
    * Replace JVM option UseCGroupMemoryLimitForHeap (removed in JDK 11) by UseContainerSupport
    
    * Add blank line at the end to remove GitHub warning

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,10 +2,10 @@ import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 
 plugins {
     application
-    kotlin("jvm") version "1.3.61"
-    id("com.github.johnrengelman.shadow") version "5.2.0"
-    id("com.github.breadmoirai.github-release") version "2.2.10"
-    id("com.github.ben-manes.versions") version "0.27.0"
+    kotlin("jvm") version "1.4.10"
+    id("com.github.johnrengelman.shadow") version "6.0.0"
+    id("com.github.breadmoirai.github-release") version "2.2.12"
+    id("com.github.ben-manes.versions") version "0.33.0"
 }
 
 version = 0
@@ -18,13 +18,13 @@ dependencies {
     testImplementation("io.kotlintest:kotlintest-runner-junit5:3.4.2")
     implementation("ch.qos.logback:logback-classic:1.2.3")
     implementation("com.neelkamath.kwikipedia:kwikipedia:0.7.2")
-    implementation("com.github.javafaker:javafaker:1.0.1")
-    val ktorVersion = "1.2.6"
+    implementation("com.github.javafaker:javafaker:1.0.2")
+    val ktorVersion = "1.4.1"
     implementation("io.ktor:ktor-server-core:$ktorVersion")
     implementation("io.ktor:ktor-server-netty:$ktorVersion")
     implementation("io.ktor:ktor-gson:$ktorVersion")
     testImplementation("io.ktor:ktor-server-test-host:$ktorVersion")
-    val retrofitVersion = "2.7.0"
+    val retrofitVersion = "2.9.0"
     implementation("com.squareup.retrofit2:retrofit:$retrofitVersion")
     implementation("com.squareup.retrofit2:converter-gson:$retrofitVersion")
 }
